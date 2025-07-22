caeffd3de10f27a075a7310cfcc0285a56094463
hi2242 <doodukly1999@gmail.com>
2025-07-22 21:33:51 +0900

    [Fix] GitHub Actions (CI) 오류 : spotlessApply를 통해 자동 수정

--- app/src/main/java/com/example/chaining/MainActivity.kt
+++ app/src/main/java/com/example/chaining/MainActivity.kt
@@ -11,18 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.chaining.ui.theme.ChainingTheme
+import com.example.chaining.ui.theme.chainingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ChainingTheme {
+            chainingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
@@ -31,17 +31,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun greeting(name: String, modifier: Modifier = Modifier) {
+fun greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun greetingPreview() {
-    ChainingTheme {
+    chainingTheme {
         greeting("Android")
     }
 }
