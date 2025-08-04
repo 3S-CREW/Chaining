7281ded55936cc788be472baa54820be835b1fc5
hi2242 <doodukly1999@gmail.com>
2025-08-04 17:12:48 +0900

    [Feat] 법정동 정보 API Retrofit 인터페이스 생성

--- app/src/main/java/com/example/chaining/network/AreaService.kt
+++ app/src/main/java/com/example/chaining/network/AreaService.kt
@@ -1,26 +1,21 @@
 package com.example.chaining.network
 
-import com.example.chaining.BuildConfig.DATA
-import com.example.tripplan.model.AreaModel
-import okhttp3.ResponseBody
-import retrofit2.Call
+import com.example.chaining.BuildConfig.DATA_OPEN_API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-// 배포시에는 안전하게 보완 적용 필요
-// 공공 데이터 포탈에서 발급 받은 자신만의 API키를 입력해 주세요.
-
+// https://apis.data.go.kr/B551011/KorService2/ldongCode2?serviceKey=DATA_OPEN_API_KEY&numOfRows=1000&lDongListYn=Y&pageNo=1&lDongRegnCd=11&MobileOS=AND&MobileApp=AppTest
 
 interface AreaService {
-    @GET("areaCode1?serviceKey=${API_KEY}")
-    suspend fun getArea(
-//        @Query("serviceKey") apiKey: String = BuildConfig.API_KEY,
-        @Query("areaCode") areaCode: String? = "", // 지역 코드
-        @Query("numOfRows") numOfRows: Int = 20,
-        @Query("pageNo") pageNo: Int = 1,
-        @Query("MobileOS") mobileOS: String = "AND",
-        @Query("MobileApp") mobileAPP: String = "TripPlan",  // 여기를 확인
-        @Query("_type") type: String = "json"
-    ): AreaModel
+    @GET("ldongCode2?serviceKey=${DATA_OPEN_API_KEY}")
+    suspend fun getAreaCodes(
+        @Query("lDongRegnCd") lDongRegnCd: Int = 11, // 지역 코드
+        @Query("PageNo") PageNo: Int = 1, // 불러올 페이지 수
+        @Query("numOfRows") numOfRows: Int = 1000, // 불러올 행의 수
+        @Query("lDongListYn") lDongListYn: String = "Y", // 목록조회 여부
+        @Query("MobileOS") mobileOS: String = "AND", // OS 종류
+        @Query("MobileApp") mobileAPP: String = "Chaining",  // 앱 명
+        @Query("_type") type: String = "json" // 데이터 타입
+    ): AreaCodeResponse
 
 }
