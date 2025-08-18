a33f45b340a362f4b67bcb112fff46866cee99be
hi2242 <doodukly1999@gmail.com>
2025-08-18 21:36:29 +0900

    [Design] 마이페이지 Route 추가

--- app/src/main/java/com/example/chaining/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/chaining/ui/navigation/Screen.kt
@@ -5,4 +5,5 @@ sealed class Screen(val route: String) {
     object Login : Screen("login")
     object Home : Screen("home")
     object Area : Screen("area")
+    object MyPage : Screen("mypage")
 }
