0096cae33191352d66daf840338adc1d773d6d11
hi2242 <doodukly1999@gmail.com>
2025-08-19 22:14:34 +0900

    [Design] 드롭다운 UI 수정

--- app/src/main/java/com/example/chaining/domain/model/User.kt
+++ app/src/main/java/com/example/chaining/domain/model/User.kt
@@ -7,7 +7,7 @@ data class User(
     val profileImageUrl: String = "",
     val country: String = "",                     // 출신국
     val residence: String = "",                   // 거주지
-    val preferredDestinations: List<String> = emptyList(), // 선호 여행지
+    val preferredDestinations: String = "",       // 선호 여행지
     val preferredLanguages: List<LanguagePref> = emptyList(), // 선호 언어 + 수준
     val isPublic: Boolean = true,                 // 모집/지원 현황 공개 여부
     val recruitPosts: List<String> = emptyList(), // 내가 모집한 글 (Post ID만 저장)
--- app/src/main/java/com/example/chaining/ui/screen/MyPageScreen.kt
+++ app/src/main/java/com/example/chaining/ui/screen/MyPageScreen.kt
@@ -1,7 +1,11 @@
 package com.example.chaining.ui.screen
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,19 +15,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -33,12 +41,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
+import com.example.chaining.R
 import com.example.chaining.domain.model.User
 import com.example.chaining.viewmodel.UserViewModel
 
@@ -56,13 +67,34 @@ fun MyPageScreen(
         ProfileSection(user = userState, onNicknameChange = { newNickname ->
             userViewModel.updateUser(mapOf("nickname" to newNickname))
         })
-
-        Spacer(modifier = Modifier.height(24.dp))
-        BasicInfoSection(
-            selectedCountry = userState?.country ?: "한국",
-            onCountryChange = { newCountry ->
-                userViewModel.updateUser(mapOf("country" to newCountry))
-            }
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = "기본 정보",
+            fontWeight = FontWeight.SemiBold,
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        DropDownField(
+            items = listOf("한국", "미국", "일본"),
+            selectedItem = userState?.country ?: "한국",
+            onItemSelected = { country -> userViewModel.updateUser(mapOf("country" to country)) },
+            leadingIconRes = R.drawable.airport,
+            placeholder = "출신 국가 선택"
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        DropDownField(
+            items = listOf("서울", "부산", "제주"),
+            selectedItem = userState?.residence ?: "서울",
+            onItemSelected = { residence -> userViewModel.updateUser(mapOf("residence" to residence)) },
+            leadingIconRes = R.drawable.country,
+            placeholder = "현재 거주지 선택"
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        DropDownField(
+            items = listOf("파리", "도쿄", "뉴욕"),
+            selectedItem = userState?.preferredDestinations ?: "파리",
+            onItemSelected = { preferredDestinations -> userViewModel.updateUser(mapOf("preferredDestinations" to preferredDestinations)) },
+            leadingIconRes = R.drawable.forest_path,
+            placeholder = "선호 여행지 선택"
         )
 
         Spacer(modifier = Modifier.height(24.dp))
@@ -118,45 +150,118 @@ fun ProfileSection(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BasicInfoSection(
-    selectedCountry: String,
-    onCountryChange: (String) -> Unit
+fun DropDownField(
+    items: List<String>,                   // 드롭다운 항목
+    selectedItem: String,                  // 선택된 값
+    onItemSelected: (String) -> Unit,      // 선택 시 콜백
+    leadingIconRes: Int,                   // 아이콘 리소스
+    placeholder: String,                   // Placeholder 텍스트
+    modifier: Modifier = Modifier
 ) {
     var expanded by remember { mutableStateOf(false) }
-    val countries = listOf("한국", "미국", "일본")
-    var country by remember { mutableStateOf(selectedCountry) }
+    val rotation by animateFloatAsState(
+        targetValue = if (expanded) 180f else 0f,
+        animationSpec = tween(durationMillis = 300)
+    )
 
-    Column {
-        Text("기본 정보", fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.height(8.dp))
+    Column(modifier = Modifier.fillMaxWidth()) {
 
         ExposedDropdownMenuBox(
             expanded = expanded,
-            onExpandedChange = { expanded = !expanded }
+            onExpandedChange = { expanded = !expanded },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFF637387),
+                    shape = RoundedCornerShape(12.dp)
+                )
         ) {
-            TextField(
-                value = country,
-                onValueChange = { },
-                label = { Text("출신 국가 선택") },
-                readOnly = true,
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded) },
-                modifier = Modifier.menuAnchor()
-            )
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
-            ) {
-                countries.forEach { c ->
-                    androidx.compose.material3.DropdownMenuItem(
-                        text = { Text(c) },
-                        onClick = {
-                            country = c
-                            onCountryChange(c)
-                            expanded = false
-                        }
-                    )
+            Crossfade(
+                targetState = selectedItem,
+                animationSpec = tween(300),
+                label = "countryCrossfade"
+            ) { animatedItem ->
+                TextField(
+                    value = animatedItem,
+                    onValueChange = { },
+                    readOnly = true,
+                    textStyle = LocalTextStyle.current.copy(
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color(0xFF637387)
+                    ),
+                    label = {
+                        Text(
+                            placeholder,
+                            fontSize = 10.sp,
+                            color = Color(0xD9637387),
+                            fontWeight = FontWeight.Medium,
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = leadingIconRes),
+                            contentDescription = null,
+                            tint = Color.Unspecified,
+                            modifier = Modifier
+                                .height(24.dp)
+                                .width(24.dp)
+                        )
+                    },
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.triangle_arrow),
+                            contentDescription = null,
+                            tint = Color.Unspecified,
+                            modifier = Modifier
+                                .height(20.dp)
+                                .width(20.dp)
+                                .rotate(rotation)
+                        )
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedTextColor = Color(0xFF637387),
+                        unfocusedTextColor = Color(0xFF637387),
+                        disabledTextColor = Color(0xFF637387),
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White,
+                        disabledContainerColor = Color.White,
+                        cursorColor = Color(0xFF637387),
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .menuAnchor()
+                        .fillMaxWidth()
+                )
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier
+                        .exposedDropdownSize()
+                        .background(Color.White)
+                ) {
+                    items.forEach { c ->
+                        DropdownMenuItem(
+                            text = {
+                                Text(
+                                    text = c,
+                                    color = Color(0xFF637387),
+                                    fontWeight = if (c == selectedItem) FontWeight.Bold else FontWeight.Normal
+                                )
+                            },
+                            onClick = {
+                                onItemSelected(c)
+                                expanded = false
+                            }
+                        )
+                    }
                 }
             }
+
         }
     }
 }
