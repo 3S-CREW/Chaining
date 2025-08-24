3a6b01f393e2db72b781dc0148506393850609b5
lhs2257 <lhs2257@gmail.com>
2025-08-24 18:39:12 +0900

    [Fix] 신청화면 디자인 조정

--- app/src/main/java/com/example/chaining/ui/screen/JoinPostScreen.kt
+++ app/src/main/java/com/example/chaining/ui/screen/JoinPostScreen.kt
@@ -10,11 +10,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
