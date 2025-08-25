02fa84234f4c93c25c2958d2bf3b3af25fe7a160
hi2242 <doodukly1999@gmail.com>
2025-08-25 23:14:28 +0900

    [Design] 모집글 조회 버튼 생성

---
+++ app/src/main/java/com/example/chaining/ui/screen/CommunityScreen.kt
@@ -0,0 +1,18 @@
+package com.example.chaining.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CommnuityScreen() {
+    Column(
+        modifier = Modifier
+            .height(20.dp)
+    ) {
+        Text(text = "Hello World")
+    }
+}
