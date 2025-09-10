11ec7cd32d9d6665dc016327142804976052d0ad
hi2242 <doodukly1999@gmail.com>
2025-09-10 15:37:02 +0900

    [Feat] 사용자 알림 데이터 클래스 설정

---
+++ app/src/main/java/com/example/chaining/domain/model/Notification.kt
@@ -0,0 +1,12 @@
+package com.example.chaining.domain.model
+
+data class Notification(
+    val id: String = "",            // 알림 ID
+    val type: String = "",          // 알림 종류
+    val postId: String? = null,     // 관련 모집글
+    val applicationId: String? = null, // 지원서일 경우 지원서 ID
+    val senderId: String? = null,   // 팔로우나 신청자 ID
+    val status: String? = null,     // 지원서 승인/거절 상태
+    val createdAt: Long = 0L,       // 타임 스탬프
+    val isRead: Boolean = false     // 읽음 여부
+)
